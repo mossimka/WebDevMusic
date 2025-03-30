c1d703fcdd81b3139e18f14621b67976a6b53a2b
mossimka <maksimsarsekeev@gmail.com>
2025-03-30 19:50:40 +0500

    sign in,up front

--- FreeBird/music-shop/src/app/sign-in/sign-in.component.ts
+++ FreeBird/music-shop/src/app/sign-in/sign-in.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import {RouterLink} from '@angular/router';
+import {SignUpComponent} from '../sign-up/sign-up.component';
 
 @Component({
   selector: 'app-sign-in',
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './sign-in.component.html',
   styleUrl: './sign-in.component.css'
 })
 export class SignInComponent {
 
+  protected readonly SignUpComponent = SignUpComponent;
 }
--- FreeBird/music-shop/src/app/sign-up/sign-up.component.ts
+++ FreeBird/music-shop/src/app/sign-up/sign-up.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-sign-up',
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.css'
 })
