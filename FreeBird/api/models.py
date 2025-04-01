f07de3c2a08b6b1b715d56bbce7c3000072ccc88
gyurt <G.Yurtayev@gmail.com>
2025-04-01 15:28:03 +0500

    changed product model, added more info

--- FreeBird/api/models.py
+++ FreeBird/api/models.py
@@ -1,11 +1,26 @@
 from django.db import models
 
 class Product(models.Model):
+    TYPE_CHOICES = [    #заглушка, надо переделать
+        ('brass', 'Brass'),
+        ('guitars', 'Guitars'),
+    ]
+    
     name = models.CharField(max_length=255)
-    price = models.FloatField()
-    photo = models.TextField()
+    price = models.PositiveIntegerField()
+    photo = models.ImageField(upload_to='products/photos/')
+    sub_photos = models.JSONField()
+    type = models.CharField(max_length=20, choices=TYPE_CHOICES)
+    description = models.TextField()
+    available_units = models.PositiveIntegerField()
+    country = models.CharField(max_length=100)
+    link = models.URLField()
+    rating = models.FloatField()
+    likes = models.PositiveIntegerField(default=0)
 
-
+    def __str__(self):
+        return self.name
+    
 class User(models.Model):
     email = models.EmailField()
     password = models.CharField(255)
