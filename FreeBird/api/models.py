d29ffb8b9e024e0a04cec94418dd206caeb47731
mossimka <maksimsarsekeev@gmail.com>
2025-04-01 10:42:32 +0500

    footer and models start

--- FreeBird/api/models.py
+++ FreeBird/api/models.py
@@ -1,4 +1,19 @@
 from django.db import models
 
 class Product(models.Model):
-    name = models.CharField()
+    name = models.CharField(max_length=255)
+    price = models.FloatField()
+    photo = models.TextField()
+
+class Order(models.Model):
+    date = models.DateTimeField()
+    products = models.ManyToManyField(Product, through='OrderItem')
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    price = models.FloatField()
+
+    class Meta:
+        unique_together = ('order', 'product') # уникальная пара заказ-продукт
