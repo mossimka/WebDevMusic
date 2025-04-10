6d3c90025ef7e7d240cb391b2abffcc3a4d2d7f5
mossimka <maksimsarsekeev@gmail.com>
2025-04-10 12:25:15 +0500

    little changes

--- FreeBird/FreeBird/settings.py
+++ FreeBird/FreeBird/settings.py
@@ -47,14 +47,18 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:4200",
 ]
 
 REST_FRAMEWORK = {
@@ -138,9 +142,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
-
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:4200",
-]
 
 AUTH_USER_MODEL = 'api.User'
