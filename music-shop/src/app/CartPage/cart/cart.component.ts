d0949ec05e5057f8099dfe51eba187933da8598c
Gleb-Y <g.yurtayev@gmail.com>
2025-04-21 19:56:31 +0500

    made order creation
    made order creation

--- music-shop/src/app/CartPage/cart/cart.component.ts
+++ music-shop/src/app/CartPage/cart/cart.component.ts
@@ -1,12 +1,14 @@
 import { Component, inject, OnInit, OnDestroy } from '@angular/core';
 import {CommonModule, NgForOf} from '@angular/common';
 import { CartItemComponent } from '../cart-item/cart-item.component';
-import { CartItem } from '../../interfaces/cart-item'
+import { CartItem } from '../../interfaces/cart-item';
 import { CartService } from '../../services/cart.service';
 import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
 import {RouterLink} from '@angular/router';
 import {RoutingButtonComponent} from '../../Buttons/routing-button/routing-button.component';
+import { OrderService } from '../../services/order.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cart',
@@ -15,17 +17,21 @@ import {RoutingButtonComponent} from '../../Buttons/routing-button/routing-butto
     CommonModule,
     NgForOf,
     CartItemComponent,
-    RoutingButtonComponent
+    RoutingButtonComponent,
+    RouterLink
   ],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.css'
 })
 export class CartComponent implements OnInit, OnDestroy {
   cartItemList: CartItem[] = [];
-  cartService: CartService = inject(CartService);
   private destroy$ = new Subject<void>();
 
-  constructor() {}
+  constructor(
+    private cartService: CartService,
+    private orderService: OrderService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.loadCartItems();
@@ -74,6 +80,37 @@ export class CartComponent implements OnInit, OnDestroy {
            this.loadCartItems();
         },
         error: (err) => console.error('Failed to update quantity:', err)
+      });
+  }
+
+  calculateTotalPrice(): number {
+    return this.cartItemList.reduce(
+      (total, item) => total + (item.product.price * item.quantity),
+      0
+    );
+  }
+  
+  createOrder(): void {
+    const orderData = {
+      items: this.cartItemList.map(item => ({
+        product: item.product.id,
+        quantity: item.quantity
+      })),
+      total_order_price: this.calculateTotalPrice()
+    };
+  
+    this.orderService.createOrder(orderData)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({
+        next: (order) => {
+          console.log('Order created:', order);
+          this.cartService.clearCart().subscribe(() => {
+            this.router.navigate(['/orders']);
+          });
+        },
+        error: (err) => {
+          console.error('Failed to create order:', err);
+        }
       });
   }
 }
